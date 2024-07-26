ab6b064bcd84429eaeb9c91ab2baa59ef44569ae
jbowen999 <julian.e.bowen@outlook.com>
2024-07-26 11:38:22 -0700

    Add project files.

---
+++ ListsDataStructure/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ListsDataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] arr = { 54, 34, 53, 75, 23, 86, 23, 76, 43, 78, 13 };
+            PrintArray(arr);
+
+            List<int> arr1 = arr.Append(2).ToList();
+
+            arr1.Add(3);
+            arr1.Add(5);
+
+
+            string[] arr2 = { "Hello", "World" };
+
+            PrintArray(arr1.ToArray());
+
+            PrintArray(arr2);
+                       
+
+
+        }
+
+        public static void PrintArray<T>(T[] arr)
+        {
+            foreach (T i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ ListsDataStructureTests/MyArrayListTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ListsDataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsDataStructure.Tests
+{
+    [TestClass()]
+    public class MyArrayListTests
+    {
+        [TestMethod()]
+        public void Append_AddSingleElement_ShouldAddToEnd()
+        {
+            //Arrange
+            MyArrayList list = new MyArrayList();
+            // Act
+            list.Append(4);
+            // Assert
+            Assert.AreEqual(list.List[0], 4);
+        }
+
+        [TestMethod()]
+        public void Append_AddMultipleElements_ShouldAddToEnd()
+        {
+            //Arrange
+            MyArrayList list = new MyArrayList();
+            // Act
+            list.Append(4);
+            list.Append(3);
+
+            // Assert
+            Assert.AreEqual(list.List[0], 4);
+            Assert.AreEqual(list.List[1], 3);
+        }
+
+        [TestMethod()]
+        public void Append_AddMoreElementsThan_ShouldAddToEnd()
+        {
+            //Arrange
+            MyArrayList list = new MyArrayList();
+            // Act
+            list.Append(4);
+            list.Append(5);
+            list.Append(4);
+            list.Append(5);
+            list.Append(4);
+            list.Append(5);
+            list.Append(4);
+            list.Append(5);
+            list.Append(4);
+            list.Append(5);
+            list.Append(42);
+            // Assert
+            Assert.AreEqual(list.List[0], 4);
+            Assert.AreEqual(list.List[list.Size - 1], 42);
+
+        }
+        [TestMethod()]
+        public void AddStart_ListIsEmpty_ShouldAddToStart()
+        {
+            //Arrange
+            MyArrayList list = new MyArrayList();
+            // Act
+            list.AddStart(4);
+            // Assert
+            Assert.AreEqual(list.List[0], 4);
+        }
+
+        [TestMethod()]
+        public void AddStart_ListHasValues_ShouldAddToStart()
+        {
+            //Arrange
+            MyArrayList list = new MyArrayList();
+            list.Append(5);
+            list.Append(4);
+            // Act
+            list.AddStart(10);
+            // Assert
+            Assert.AreEqual(list.List[0], 10);
+        }
+    }
+}
